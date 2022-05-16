68f4e4f625fc89646a6819fcd74906eb93e75347
Adam Sciegaj <adam.sciegaj@gmail.com>
2022-05-16 12:12:26 +0200

    Added example numeric input

--- src/speedEST.py
+++ src/speedEST.py
@@ -4,3 +4,6 @@ st.write("""
 # speedEST
 Vehicle speed estimation at impact with a steel road barrier using Machine Learning
 """)
+
+mass = st.number_input('Enter the mass of the vehicle', min_value=1100.0, max_value=2000.0)
+st.write("Current mass set to ", mass)
